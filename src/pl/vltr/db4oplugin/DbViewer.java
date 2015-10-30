f6452097f6069cd9f941a798b7d5f94247093c0c
vltRz00r <vltr@o2.pl>
2015-10-30 13:39:02 +0100

    editing WIP

--- src/pl/vltr/db4oplugin/DbViewer.java
+++ src/pl/vltr/db4oplugin/DbViewer.java
@@ -165,9 +165,9 @@ public class DbViewer {
                     if (go.getGenericClass().equals(rc)) {
                         return true;
                     }
-                    if( go.getGenericClass().getSuperclass().equals(rc)){
+                    /*if( go.getGenericClass().getSuperclass().equals(rc)){
                         return true;
-                    }
+                    }*/
                 }
                 return false;
             }
